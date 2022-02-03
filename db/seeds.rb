24384b1ceb7a8052ccb252e6c61ae58a446a22dc
stanley-tarce <stanleytarce18@gmail.com>
2022-02-03 14:01:34 +0800

    Added changes in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 include GeneratorHelper
+
+User.create(email: "admin123@gmail.com", password: "Admin123", password_confirmation: "Admin123")
+
 50.times do
     Account.create(account_number: accountnum, account_type: accounttype, last_name: Faker::Name.last_name, first_name: Faker::Name.first_name, middle_name: Faker::Name.middle_name, birth_date: Faker::Date.birthday(min_age: 18, max_age:80), contact_number: contactnumber, email: Faker::Internet.email, home_address: Faker::Address.secondary_address, city: Faker::Address.city, balance: balance, zip_code: zipcode, gender: Faker::Gender.binary_type )
 end
